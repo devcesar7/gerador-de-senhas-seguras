5c7a3699752482786cc3cb40f0f8cac78aa370d6
devcesar7 <devcesaraugusto97@gmail.com>
2025-04-01 17:01:27 -0300

    Gerador de Senhas Aleatorias

---
+++ gerador-de-senhas.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+# Define os tipos de caracteres disponíveis
+letras_maiusculas = string.ascii_uppercase
+letras_minusculas = string.ascii_lowercase
+numeros = string.digits
+simbolos = string.punctuation
+
+# Junta todos os caracteres possíveis
+conjunto_de_caracteres = letras_maiusculas + letras_minusculas + numeros + simbolos
+
+# Solicita o tamanho da senha ao usuário
+tamanho_senha = int(input("Digite o tamanho da senha: "))
+
+# Gera uma lista de caracteres aleatórios com o tamanho especificado
+lista_de_caracteres = random.choices(conjunto_de_caracteres, k=tamanho_senha)
+
+# Converte a lista de caracteres em uma string
+senha_gerada = "".join(lista_de_caracteres)
+
+# Exibe a senha gerada
+print("Sua senha segura é:", senha_gerada)
